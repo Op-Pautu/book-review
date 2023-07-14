4ce4672364a3de2b7c81b179c228c0bacc00b79d
Op-Pautu <pautuop@gmail.com>
2023-07-15 02:09:09 +0530

    Add rate limiter

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -2,10 +2,13 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Add Review for {{ $book->title }}</h1>
 
-    <form method="POST" action="{{ route('books.reviews.store', $book) }}">
+<form method="POST" action="{{ route('books.reviews.store', ['book' => $book]) }}">
     @csrf
     <label for="review">Review</label>
     <textarea name="review" id="review" required class="input mb-4"></textarea>
+    @error('review')
+        <p class="text-red-600 mb-4">{{ $message }}</p>
+    @enderror
 
     <label for="rating">Rating</label>
 
@@ -15,6 +18,9 @@
             <option value="{{ $i }}">{{ $i }}</option>
         @endfor
     </select>
+    @error('rating')
+        <p class="text-red-600">{{ $message }}</p>
+    @enderror
 
     <button type="submit" class="btn">Add Review</button>
     </form>
